8f3cc28069012cf8cdb3f607e15d2e3663c5b144
PC <amolujagare@gmail.com>
2021-06-08 20:28:42 +0530

    darksky

--- src/main/java/pages/DarkSkyHomePage.java
+++ src/main/java/pages/DarkSkyHomePage.java
@@ -1,11 +1,51 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+import java.util.ArrayList;
+
+import static stepdefinitions.SharedSD.getDriver;
 
 public class DarkSkyHomePage extends Base{
 
     By tempRowText = By.xpath("//span[@class='summary swap']"); //99˚ Clear.
     By timelineTempRowText = By.xpath("//span[@class='first']//span"); // 99°
+    By timeRowList = By.xpath("//span[@class='hour']/span");
+    By todaysTemp = By.xpath("//a[@data-day='0']");
+
+    public void clickTodayTemp()
+    {
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("window.scrollBy(0,900)");
+
+        clickOn(todaysTemp);
+    }
+
+
+
+    public ArrayList<Integer> getTimeList()
+    {
+        ArrayList<String> timeList = getElementTextList(timeRowList);
+        System.out.println(timeList);
+
+        ArrayList<Integer> timeListInt = new ArrayList<>();
+        for(int i=0;i<timeList.size();i++)
+        {
+            String timeStrRow = timeList.get(i); // 9pm
+
+            int l = timeStrRow.length();
+
+              String timeStr =  timeStrRow.substring(0,l-2);
+              int time = Integer.parseInt(timeStr);
+
+                timeListInt.add(time);
+        }
+
+        return timeListInt;
+    }
+
+
 
     public int getTemp()
     {
--- src/main/java/stepdefinitions/DarkskySD.java
+++ src/main/java/stepdefinitions/DarkskySD.java
@@ -5,6 +5,9 @@ import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import pages.DarkSkyHomePage;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import static stepdefinitions.SharedSD.getDriver;
 
 public class DarkskySD {
@@ -34,4 +37,46 @@ public class DarkskySD {
                  Assert.assertEquals("Temperatures are not equal",
                          expected,actual);
     }
+
+    @Then("^I verify timeline is displayed with two hours incremented$")
+    public void i_verify_timeline_is_displayed_with_two_hours_incremented()
+            {
+                ArrayList<Integer> timelist = darkSkyHomePage.getTimeList();
+                System.out.println(timelist);
+                //[9, 11, 1, 3, 5, 7, 9, 11, 1, 3, 5] --> size is 11 (last index 10)
+                ArrayList<Integer> timeDiffList = new ArrayList<>();
+
+                for(int i=0;i<timelist.size()-1;i++)
+                {
+                    int time1 = timelist.get(i);
+                    int time2 = timelist.get(i+1);
+
+                    int timeDiff = 0;
+                    if(time2>time1)
+                      timeDiff = time2-time1;
+                    if(time1>time2)
+                        timeDiff = (time2+12) - time1;
+
+                    timeDiffList.add(timeDiff);
+                }
+                System.out.println(timeDiffList);
+
+                int size = timeDiffList.size();
+
+                int occarance = Collections.frequency(timeDiffList,2);
+
+                boolean result = (size == occarance);
+
+                Assert.assertTrue("all differences are not 2",result);
+
+    }
+
+
+    @Then("^I verify today's lowest and highest temp is displayed correctly$")
+    public void i_verify_todays_lowest_and_highest_temp_is_displayed_correctly()
+    {
+        darkSkyHomePage.clickTodayTemp();
+    }
+
+
 }
